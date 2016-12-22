07cbb4a1fb56fb8e1e35aac4c560b530c6bd2c3a
RushOneGit <RushOneGit@users.noreply.github.com>
2016-12-22 20:34:30 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-var autoPrefixBrowserList = ['last 4 version', 'safari 8', 'ie 11', 'opera 12.1', 'ios 6', 'android 4'];
+var autoPrefixBrowserList = ['last 2 version'];
 var gulp                  = require('gulp');
 var gutil                 = require('gulp-util');
 var concat                = require('gulp-concat');
